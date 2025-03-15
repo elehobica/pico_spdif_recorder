89c58e6683c85ccf6531d26f6833425a8a2192ac
elehobica <elehobica@gmail.com>
2025-03-15 02:53:46 +0000

    revise ConfigParam.h for CFG_ID_xxx

--- src/ConfigParam.h
+++ src/ConfigParam.h
@@ -10,17 +10,20 @@
 
 #include "FlashParam.h"
 
+typedef enum {
+    CFG_ID_WIFI_SSID = FlashParamNs::CFG_ID_BASE,
+    CFG_ID_WIFI_PASS,
+} ParamId_t;
+
 //=================================
 // Interface of ConfigParam class
 //=================================
 struct ConfigParam : FlashParamNs::FlashParam {
-    static ConfigParam& instance()  // Singleton
-    {
+    static ConfigParam& instance() {  // Singleton
         static ConfigParam instance;
         return instance;
     }
-    static constexpr uint32_t ID_BASE = FlashParamNs::CFG_ID_BASE;
     // Parameter<T>                      instance        id           name             default size
-    FlashParamNs::Parameter<std::string> P_CFG_WIFI_SSID{ID_BASE + 0, "CFG_WIFI_SSID", "",     16};
-    FlashParamNs::Parameter<std::string> P_CFG_WIFI_PASS{ID_BASE + 1, "CFG_WIFI_PASS", "",     16};
+    FlashParamNs::Parameter<std::string> P_CFG_WIFI_SSID{CFG_ID_WIFI_SSID, "CFG_WIFI_SSID", "",     16};
+    FlashParamNs::Parameter<std::string> P_CFG_WIFI_PASS{CFG_ID_WIFI_PASS, "CFG_WIFI_PASS", "",     16};
 };

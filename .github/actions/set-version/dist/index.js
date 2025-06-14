9f488e6bd4cbebec9e56e4cb227568e7a4708236
elehobica <elehobica@gmail.com>
2025-06-14 04:21:29 +0000

    add regexp_str for set-version action

--- .github/actions/set-version/dist/index.js
+++ .github/actions/set-version/dist/index.js
@@ -27556,7 +27556,7 @@ module.exports = parseParams
 /************************************************************************/
 var __webpack_exports__ = {};
 /*-----------------------------------------------------------/
-/ index.js
+/ set-version/index.js
 /------------------------------------------------------------/
 / Copyright (c) 2025, Elehobica
 / Released under the BSD-2-Clause
@@ -27572,7 +27572,12 @@ try {
   let version_str = core.getInput('version_str', {required: true});
   let version_str_size = core.getInput('version_str_size', {required: true});
   version_str = version_str.length > version_str_size ? version_str.slice(0, version_str_size) : version_str;
-  fs.writeFileSync(targetFile, text.replace(/("CFG_VERSION",\s+)"\d\.\d\.\d"/, `$1"${version_str}"`));
+  let regExp = new RegExp(core.getInput('regexp_str', {required: true}));
+  //let regExp = new RegExp('("CFG_ID_VERSION",\\s+)"\\d\.\\d\.\\d"');
+  //let regExp = /("CFG_ID_VERSION",\s+)"\d\.\d\.\d"/;
+  fs.writeFileSync(targetFile, text.replace(regExp, `$1"${version_str}"`));
+  //fs.writeFileSync(targetFile, text.replace(/("CFG_ID_VERSION",\s+)"\d\.\d\.\d"/, `$1"${version_str}"`));
+  //fs.writeFileSync(targetFile, text.replace(/("CFG_VERSION",\s+)"\d\.\d\.\d"/,  `$1"${version_str}"`));
 } catch (error) {
     core.setFailed(error.message);
 }
